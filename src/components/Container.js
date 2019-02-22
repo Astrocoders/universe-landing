3896a48ef124ea648d0df2da42c845fd1b6ff485
Jefferson <jeffersonhcarvalho@gmail.com>
2019-02-22 10:15:47 -0300

    Added jest

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = () => <Div />;
+
+export default Container;
