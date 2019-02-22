f2f27fdb8a82f90c86eefd4a17214dda5002f946
Jefferson <jeffersonhcarvalho@gmail.com>
2019-02-22 09:02:15 -0300

    Added styled-components and react-helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
 
-const IndexPage = () => <h1> Hello, World! </h1>;
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Header = styled.h1`
+  font-family: Roboto;
+`;
+
+const IndexPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <Container>
+      <Header>Hello, World!</Header>
+    </Container>
+  </React.Fragment>
+);
 
 export default IndexPage;
