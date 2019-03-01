12379a6684451bc4a41fe513fa22950c68b4b808
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-01 11:14:32 -0300

    test(ListItem): add ListItem tests

---
+++ src/components/ListItem.test.tsx
@@ -0,0 +1,22 @@
+import 'jest-dom/extend-expect'
+import React from 'react'
+import { cleanup, render } from 'react-testing-library'
+
+import ListItem, { IProps } from './ListItem'
+
+afterEach(cleanup)
+
+const props: IProps = {
+  title: "ListItem's title",
+  description: "ListItem's description"
+}
+
+describe('ListItem test', () => {
+  it('should render the ListItem', () => {
+    const { getByTestId, getByText } = render(<ListItem {...props} />)
+
+    expect(getByTestId('list-item')).toBeVisible()
+    expect(getByText(props.title)).toBeVisible()
+    expect(getByText(props.description)).toBeVisible()
+  })
+})
