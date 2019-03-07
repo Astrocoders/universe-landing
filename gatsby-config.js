8e1596a0b2ee09d869a657eef03a6844c9b1f094
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-07 09:58:51 -0300

    chore: add gatsby-plugin-offline and gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,19 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "Universe",
+        short_name: "Universe",
+        background_color: "#e5e5e5",
+        theme_color: "#8773e3",
+        description: "Find ReasonML bulletproof bindings",
+        display: "minimal-ui",
+        icon: "src/images/astro-logo-micro.png"
+      }
+    },
+    'gatsby-plugin-offline',
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Roboto\: 300,400,700`],
