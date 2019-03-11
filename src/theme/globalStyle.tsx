9fa5e45bf8305b4840c35ff521cfd0cbf5341d0a
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-11 11:42:11 -0300

    fix: add 100% height to page body

--- src/theme/globalStyle.tsx
+++ src/theme/globalStyle.tsx
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    height: 100%;
     background-color: #e5e5e5;
     font-family: Roboto;
     font-size: 12px;
